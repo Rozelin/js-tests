e791a6b718fe5c7ffd6865cb5417d78e8315204f
Olena Rozumna <o.rozumna@astoundcommerce.com>
2017-04-05 12:28:10 +0300

    Native JS practical tasks - Textarea symbols counting

---
+++ counter/app.js
@@ -0,0 +1,20 @@
+var TextCounter = {
+    init: function() {
+        this.textArea = document.querySelector('#js-message');
+        this.maxChar = +this.textArea.getAttribute('maxlength') || 140;
+        this.textAreaTotal = document.querySelector('#js-message-left-total');
+        this.textAreaLeft = document.querySelector('#js-message-left-symbols');
+        this.textAreaLeft.textContent = this.maxChar;
+        this.events();
+    },
+
+    events: function() {
+      this.textArea.addEventListener('keydown', function(e){
+        if(this.value.length > TextCounter.maxChar) {
+          this.value = this.value.slice(0, -1);
+        }
+        TextCounter.textAreaTotal.textContent = this.value.length;
+        TextCounter.textAreaLeft.textContent = TextCounter.maxChar - this.value.length;
+        }
+    )}
+};
