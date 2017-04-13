86aa2109b985f6b714581dd6b14d5291529ffefe
Olena Rozumna <o.rozumna@astoundcommerce.com>
2017-04-13 17:27:05 +0300

    Added changes according to code review

--- tableEditor/app.js
+++ tableEditor/app.js
@@ -2,6 +2,7 @@
 
 var tableEditorModule = (function() {
   const table           = document.getElementById("tableBody");
+  const pagination      = document.getElementById("pagination");
   const addRowBtn       = document.getElementById("addRow");
   const addRowForm      = document.getElementById("addRowForm");
   const clearTableBtn   = document.getElementById("clearTable");
@@ -19,11 +20,14 @@ var tableEditorModule = (function() {
   const filterNameInp   = document.getElementById("filterName");
   const initStr         = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const strLength       = initStr.length;
+  const paginationLen   = 10;
 
-  let tableData         = [{"id":1,"name":"dfj3RnZGcI","qty":951,"avail":"yes"},{"id":2,"name":"VyoHrnaa","qty":165,"avail":"yes"},{"id":3,"name":"Xlb","qty":78,"avail":"no"},{"id":4,"name":"RQ9hJ","qty":686,"avail":"no"},{"id":5,"name":"oM0mlnsj","qty":660,"avail":"yes"},{"id":6,"name":"KBZc","qty":62,"avail":"no"},{"id":7,"name":"TQpq0cG","qty":142,"avail":"yes"},{"id":8,"name":"NO48Wp4","qty":314,"avail":"no"},{"id":9,"name":"odE1AC","qty":305,"avail":"yes"},{"id":10,"name":"o9z2iNDM","qty":891,"avail":"no"},{"id":11,"name":"0S9undefinedP3V","qty":810,"avail":"yes"},{"id":12,"name":"9gC0B5iWa","qty":741,"avail":"no"},{"id":13,"name":"ptfylSw","qty":213,"avail":"yes"},{"id":14,"name":"Ym7GL4","qty":532,"avail":"no"},{"id":15,"name":"cYjFXiWra","qty":720,"avail":"yes"},{"id":16,"name":"7MtvF435ay","qty":330,"avail":"yes"},{"id":17,"name":"zLd8UAT","qty":113,"avail":"no"},{"id":18,"name":"uUWT2","qty":42,"avail":"yes"},{"id":19,"name":"s6f3okg","qty":202,"avail":"yes"},{"id":20,"name":"9WFrp5","qty":333,"avail":"no"},{"id":21,"name":"YAlE","qty":216,"avail":"yes"},{"id":22,"name":"HuZs2SAAV","qty":386,"avail":"yes"},{"id":23,"name":"fO8J","qty":573,"avail":"yes"},{"id":24,"name":"V6s","qty":30,"avail":"yes"},{"id":25,"name":"wundefinedqlAx","qty":918,"avail":"no"},{"id":26,"name":"39QpLqzgsQ","qty":386,"avail":"yes"},{"id":27,"name":"vPG1h1M","qty":746,"avail":"yes"},{"id":28,"name":"xK6SGJjdcQ","qty":576,"avail":"no"},{"id":29,"name":"JkeIn9T3","qty":794,"avail":"yes"},{"id":30,"name":"U5NnGBUBq8","qty":793,"avail":"yes"}];
+  let tableData         = [];
+  let selectedTable     = [];
 
-  function drawTable(arr) {
-    arr = arr || tableData;
+  function drawTable(arr, num = 0) {
+    arr = arr || tableData.slice(num, 10);
+
     let newTableBody = arr.map(function(row) {
       return `
         <tr>
@@ -32,10 +36,32 @@ var tableEditorModule = (function() {
           <td>${row.qty}</td>
           <td>${row.avail}</td>
           <td><input type="checkbox"></td>
-        </tr>`}).join('');
+        </tr>`}).join("");
 
     table.innerHTML = newTableBody;
-};
+    addPagination(tableData);
+  };
+
+  function addPagination(arr) {
+    let paginationList = "";
+    let pageNum = Math.ceil(arr.length / paginationLen);
+    for (var i = 0; i < pageNum; i++) {
+      paginationList += `
+        <li><a href="#${i+1}">${i+1}</a></li>
+        `;
+      }
+      pagination.innerHTML = paginationList;
+    };
+
+    pagination.addEventListener("click", function(e) {
+      if (e.target.tagName === "A") {
+        e.preventDefault();
+        let start = +e.target.href.split("#")[1] * 10 - 10;
+        let end   = +start + 10
+        selectedTable = tableData.slice(start, end);
+        drawTable(selectedTable);
+      }
+    });
 
   function addNewRow() {
     addRowBtn.addEventListener("click", function() {
@@ -64,7 +90,6 @@ var tableEditorModule = (function() {
       drawTable();
     });
   };
-
   function recountIds() {
     tableData.map(function(item, i) {
       item.id = i + 1;
@@ -74,10 +99,17 @@ var tableEditorModule = (function() {
   function delRows() {
     delRowsBtn.addEventListener("click", function(e) {
       e.preventDefault();
-      let checkedRows = table.querySelectorAll("input[type=checkbox]:checked");
-      checkedRows.forEach(function (checkedRow, i) {
-        tableData.splice(i, 1);
+      let tableRows = table.querySelectorAll("tr");
+      let deleteRawsId = [];
+      tableRows.forEach(function (tabelRow, i) {
+        if (tabelRow.querySelector("input[type=checkbox]:checked")) {
+          deleteRawsId.push(i + 1);
+        }
       });
+      for (var len = deleteRawsId.length-1, i = len; i >= 0; i--) {
+        let index = deleteRawsId[i] -1 ;
+        tableData.splice(index, 1);
+      }
       recountIds();
       drawTable();
     });
@@ -87,7 +119,7 @@ var tableEditorModule = (function() {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  };
 
   function getRandString() {
     let finalStr = "";
@@ -97,30 +129,34 @@ var tableEditorModule = (function() {
     };
     return finalStr;
   };
+  function addRandomContent() {
+    let randomeRowsNum = getRandom(1, 10);
+
+    for (let i = 0; i < randomeRowsNum; i++) {
+      let randomAvail   = (getRandom(0, 1)) ? "yes" : "no";
+      let randomQty     = getRandom(0, 1000);
+      let randomName    = getRandString();
+      let newDataSet = {
+        "id"    : tableData.length + 1,
+        "name"  : randomName,
+        "qty"   : randomQty,
+        "avail" : randomAvail
+      };
+      tableData.push(newDataSet);
+    };
+    drawTable();
+  };
 
   function addDemoData() {
     demoDataBtn.addEventListener("click", function(e) {
       e.preventDefault();
-      let randomeRowsNum = getRandom(1, 10);
-
-      for (let i = 0; i < randomeRowsNum; i++) {
-        let randomAvail   = (getRandom(0, 1)) ? "yes" : "no";
-        let randomQty     = getRandom(0, 1000);
-        let randomName    = getRandString();
-        let newDataSet = {
-          "id"    : tableData.length + 1,
-          "name"  : randomName,
-          "qty"   : randomQty,
-          "avail" : randomAvail
-        };
-        tableData.push(newDataSet);
-      };
-      drawTable();
+      addRandomContent();
     });
   };
 
   function exportTableData() {
-    exportTableBtn.addEventListener("click", function() {
+    exportTableBtn.addEventListener("click", function(e) {
+      e.preventDefault();
       exportTableForm.classList.toggle("hidden");
     });
   };
@@ -164,12 +200,12 @@ var tableEditorModule = (function() {
     });
     filterNameInp.addEventListener("focusout", function() {
       this.value = "";
-      drawTable();
+      drawTable(selectedTable);
     });
   };
 
   function init() {
-    drawTable();
+    addRandomContent();
     addNewRow();
     exportTableData();
     clearTable();
